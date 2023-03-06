9478a18c5a4d55099d04657117ec56319bf30079
Maksims Jakusevs <124579162+maksimsjakusevs@users.noreply.github.com>
2023-03-06 06:40:38 +0000

    commit

--- tree_height.py
+++ tree_height.py
@@ -1,33 +1,63 @@
 # python3
+#221RDB376 Maksims Jkusevs 17.grupa
 
 import sys
 import threading
 import numpy
 
 
+def path_root(node, parents, statuses, root):
+    
+    if parents[node] == -1:
+        return 1
+    
+    else:
+        if statuses[parents[node]]:
+            return 1 + root[parents[node]]
+        
+        else:
+            root[node] = 1 + path_root(parents[node], parents, statuses, root)
+            statuses[node] = True
+            return root[node]
+        
+
 def compute_height(n, parents):
-    # Write this function
     max_height = 0
-    # Your code here
+    root = numpy.zeros(n)
+    statuses = numpy.full(n, False)
+
+    for i in range(n):
+        root[i] = path_root(i, parents, statuses, root)
+    max_path = root[0]
+    
+    for root in root[1:]:
+        if root > max_path:
+            max_path = root
+    max_height = int(max_path)
+
     return max_height
 
 
 def main():
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    pass
+    type = input()
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
+    if "i" in type.lower():
+        count = int(p_input())
+        p_input = str(p_input())
+        list = [int(num) for num in p_input.split(" ")]
+        print(compute_height(count, list))
+
+    elif "f" in type.lower():
+        file_name = str(p_input())
+        path = "test/" + file_name
+
+        if not "a" in path:
+            with open(path, 'r') as file:
+                count = int(file.readline())
+                p_input = str(file.readline())
+                list = [int(num) for num in p_input.split(" ")]
+                print(compute_height(count, list))
+
+sys.setrecursionlimit(10**7)  
+threading.stack_size(2**27)   
 threading.Thread(target=main).start()
-main()
-# print(numpy.array([1,2,3]))
